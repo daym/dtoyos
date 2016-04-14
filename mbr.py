122282c12d98f272f342472e8f1b200446825441
Danny Milosavljevic <dannym@scratchpost.org>
2016-04-14 19:49:41 +0200

    Add mbr generator.

---
+++ mbr.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import struct
+# load addr: 0x7c00, cs=0. OK registers: CS, SS, SP, DL (this one contains the drive letter)
+# mem map: < 0x400: interrupt vector table
+# then < 0x500: BIOS reserved RAM
+
+data = [
+	0x8c, 0xc8, # mov %cs -> %ax
+	0x8e, 0xd8, # mov %ax -> %ds
+	0x8e, 0xc0, # mov %ax -> %es
+	0x90, 0x90, # nop nop # or: 0xb2, 0x80, # mov $0x80 -> %dl
+	0xbe, 0x19, 0x7c, # mov $DAP_header -> %si
+	0xb4, 0x42, # mov 0x42 -> %ah
+	0xcd, 0x13, # int $0x13
+	0x72, 0x05, # jc hang
+	0xea, 0x00, 0x02, 0x50, 0x00, # jmp 0x0050:0x0200
+	# hang: [TODO print error]
+	0xf4, # hlt # TODO better: 0xcd, 0x18, # int 18h # return to BIOS
+	0xeb, 0xfd, # jmp hang
+	# DAP_header:
+	0x10, # size of DAP
+	0x00, # unused
+	3, 0, # number of sectors to read
+	0x00, 0x00, 0x50, 0x00, # offset, segment of destination buffer
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # starting sector
+]
+#print("LEN", len(data))
+assert(data.index(0x10) == 0x19)
+while len(data) < 0x1B8:
+	data.append(0)
+data += [
+	0xAF, 0xFE, 0x10, 0x13, # disk id
+	0x00, 0x00,
+	# partition table
+	0x40, 0x50, # magic
+	0x00, 0x00, # reserved
+	0x00, 0x00, 0x00, 0x00, # number of partitions
+]
+print(len(data))
+while len(data) < 0x1FE:
+	data.append(0)
+data += [
+	0x55, 0xAA, # boot signature
+]
+#DAP_header_ref_off = data.index(0xbe) + 1
+
+#print(hang_off)
+with open("mbr.raw", "wb") as f:
+	f.write(b"".join(struct.pack("B", x) for x in data))
